3c8c1f6777335d515f0c27d9a9764ed9d83dae24
Nabil Almrorstani <nabilalmorstani@hotmail.com>
2023-03-23 15:05:52 +0100

    .

--- src/api.js
+++ src/api.js
@@ -5,11 +5,17 @@ const app = express();
 const router = express.Router();
 
 router.get("/a", (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
   res.json({
     Audi: {
       LogoLink: "",
@@ -1641,6 +1647,16 @@ router.get("/a", (req, res) => {
   });
 });
 router.get("/b", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.json({
     BMW: {
       LogoLink: "",
@@ -2342,6 +2358,16 @@ router.get("/b", (req, res) => {
   });
 });
 router.get("/m", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.json({
     Mercedes: {
       LogoLink: "",
