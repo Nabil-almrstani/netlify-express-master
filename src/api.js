6fdd2bf420abd5738382ca9e3b79a600fc6bae1a
Nabil Almrorstani <nabilalmorstani@hotmail.com>
2023-03-23 15:02:58 +0100

    ,

--- src/api.js
+++ src/api.js
@@ -5,6 +5,10 @@ const app = express();
 const router = express.Router();
 
 router.get("/a", (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true);
   
   res.json({
     Audi: {
